2c171b39b4c26b4022c207ca4d846cf7d9bcf802
iXhaha <ifangsta1102@gmail.com>
2021-02-15 11:15:24 +1100

    Create bot_preset.py

---
+++ bot_preset.py
@@ -0,0 +1,12 @@
+import discord, discord.ext, random, os, json
+from Pchecks import checks
+from discord.ext import commands, tasks
+from itertools import cycle
+
+bot = commands.Bot(command_prefix = "p!")
+
+```
+INSERT CODE HERE
+```
+
+bot.run("token")
